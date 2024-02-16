6b0f65d296ef8b0584f03496fb76012c2883225d
m.smm2003mail.ru@gmail.com <m.smm2003mail.ru@gmail.com>
2024-02-16 17:55:40 +0300

    add lab1

---
+++ 1/Valuator/Pages/Summary.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Logging;
+
+namespace Valuator.Pages;
+public class SummaryModel : PageModel
+{
+    private IDistributedCache _cache;
+    private readonly ILogger<SummaryModel> _logger;
+
+    public SummaryModel(ILogger<SummaryModel> logger, IDistributedCache cache)
+    {
+        _logger = logger;
+        _cache = cache;
+    }
+
+    public double Rank { get; set; }
+    public double Similarity { get; set; }
+
+    public void OnGet(string id)
+    {
+        _logger.LogDebug(id);
+
+        string similarityKey = "SIMILARITY-" + id;
+
+        Similarity = double.Parse(_cache.GetString(similarityKey));
+
+        string rankKey = "RANK-" + id;
+        Rank = double.Parse(_cache.GetString(rankKey));
+
+        //TODO: проинициализировать свойства Rank и Similarity значениями из БД
+    }
+}
