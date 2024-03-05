acdb89efa23117609986431fa952c94455cf00b0
m.smm2003mail.ru@gmail.com <m.smm2003mail.ru@gmail.com>
2024-03-05 08:34:32 +0300

    поправил замечания

--- 2/scripts/stop.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-echo Stopping web application on all ports
-taskkill /F /IM dotnet.exe 
-
-echo Stopping Nginx proxy server...
-taskkill /F /IM nginx.exe
-
-echo All services stopped.
---
+++ scripts/start.bat
@@ -0,0 +1,14 @@
+@echo off
+
+cd /D ../Valuator
+echo Starting web application on port 5001...
+start "" dotnet run --urls "http://127.0.0.1:5001"
+
+echo Starting another web application on port 5002...
+start "" dotnet run --urls "http://127.0.0.1:5002"
+
+echo Starting Nginx proxy server...
+
+
+start "" /D "../nginx" nginx.exe
+echo это start.bat
